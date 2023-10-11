28319328dc7bdc7d40605b41629557ff854f1740
Haseeb ur Rehman <haseeburrehman763@gmail.com>
2023-10-12 01:07:37 +0500

    Initial commit

---
+++ app.js
@@ -0,0 +1,36 @@
+// // es5
+// document.write("My name is haseeb"+"<br>")
+
+// // es6
+// document.write(`My name is haseeb`)
+
+// es5
+// var h = "haseeb"
+// var b = "ur rehman"
+// document.write("My first name is "+ h+  " and last name is "+b+"<br>")
+
+// // es6
+// document.write(`My first name is ${h} and last name is ${b}`)
+
+
+// es6    
+// var f = "haseeb"
+//     var l = "ur rehman"
+// function name(firstName , lastname ) {
+
+// return `${f} ${l}`
+
+// }
+// var greet = `hello ${name(f,l)}`
+// console.log(greet)
+
+
+// es6
+// linebreak and spaces 
+var a = "haseeb"
+var b= "ur rehman"
+var s = `hello
+        ${a} 
+               ${b}`
+document.write(s)
+                      console.log(s)
